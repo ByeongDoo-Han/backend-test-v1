fce892af66143af8baa47fc1dbb2b80bf947225f
byeongdoohan <quden04@gmail.com>
2025-10-19 00:49:43 +0900

    feat: pg excetion 클래스 추가

---
+++ modules/external/pg-client/src/main/kotlin/im/bigs/pg/external/exception/TestPgException.kt
@@ -0,0 +1,7 @@
+package im.bigs.pg.external.exception
+
+import im.bigs.pg.application.pg.port.out.TestPgErrorResult
+
+class TestPgException (
+    val errorResult: TestPgErrorResult
+): RuntimeException(errorResult.message)
